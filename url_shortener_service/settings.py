770e24abf780f734f3f6105e019d50160fc277ce
mykolamateichuk <mateichukmykola@gmail.com>
2024-02-06 17:36:54 +0200

    Add needed apps

--- url_shortener_service/settings.py
+++ url_shortener_service/settings.py
@@ -9,12 +9,18 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    # Default apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Custom apps
+    "url_shortener",
+    "tracking",
+    "accounts",
 ]
 
 MIDDLEWARE = [
